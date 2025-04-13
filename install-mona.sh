76473d86d0fc4d7d94d80bc2ce7fd697e376f99e
Crashboogie <crash2720@outlook.com>
2025-04-12 17:57:37 -0700

    Create install-mona.sh
    initial create

---
+++ install-mona.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+TOOLS=("https://github.com/corelan/windbglib/raw/master/pykd/pykd.zip" "https://github.com/corelan/windbglib/raw/master/windbglib.py" "https://github.com/corelan/mona/raw/master/mona.py" "https://www.python.org/ftp/python/2.7.17/python-2.7.17.msi" "https://download.microsoft.com/download/2/E/6/2E61CFA4-993B-4DD4-91DA-3737CD5CD6E3/vcredist_x86.exe" "https://raw.githubusercontent.com/crashboogie/OSED_scripts/refs/heads/main/install-mona.ps1" "https://github.com/lololosys/windbg-theme/raw/refs/heads/master/dark.wew")
+
+TMPDIR=$(mktemp -d)
+SHARENAME="mona-share"
+SHARE="\\\\tsclient\\$SHARENAME"
+
+trap "rm -rf $TMPDIR" SIGINT 
+
+pushd $TMPDIR >/dev/null
+
+echo "[+] once the RDP window opens, execute the following command in an Administrator terminal:"
+echo
+echo "powershell -c \"cat $SHARE\\install-mona.ps1 | powershell -\""
+echo
+
+for tool in "${TOOLS[@]}"; do
+    echo "[=] downloading $tool"
+    wget -q "$tool"
+done
+
+unzip -qqo pykd.zip
+
+rdesktop ${1} -u Administrator -p lab -r disk:$SHARENAME=.
